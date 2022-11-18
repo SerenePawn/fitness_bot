537dffb063dc67f0e931c1a41e682f56984acdbf
SerenePawn <xkirik@list.ru>
2022-11-18 18:24:45 +0300

    updated db

--- core/app.py
+++ core/app.py
@@ -1,5 +1,4 @@
 from core.db.core_db import DatabaseManager
-from core.db.models_db import SimplifiedQueries
 from templates.loader import LangManager as LM
 from aiogram import (
     Bot,
@@ -9,14 +8,13 @@ from aiogram import (
 
 class State:
     def __init__(self, token: str):
-        self.db_mgr: DatabaseManager = DatabaseManager()
-        self.sq: SimplifiedQueries = SimplifiedQueries(self.db_mgr)
+        self.db: DatabaseManager = DatabaseManager()
         self.lang_mgr: LM = LM()
         self.bot: Bot = Bot(token=token)
         self.dp: Dispatcher = Dispatcher(self.bot)
 
     async def init(self):
-        await self.db_mgr.init_connection()
+        await self.db.init_connection()
 
     async def shutdown(self):
-        await self.db_mgr.shutdown()
+        await self.db.shutdown()
--- core/db/core_db.py
+++ core/db/core_db.py
@@ -1,7 +1,12 @@
 from databases import Database
 from databases.backends.postgres import Record
-from typing import Union
+from typing import (
+    Union,
+    Any,
+    List
+)
 import logging
+from pydantic import BaseModel
 
 from core.settings import (
     POSTGRES_PASSWD,
@@ -44,14 +49,15 @@ class DatabaseManager:
         db = self.conn
         await db.execute(sql)
 
-    async def execute_many(self, sql: str, values: list[dict] | None = None):
+    async def execute_many(self, sql: str, values: List[dict] | None = None):
         if values is None:
             values = [{}]
         db = self.conn
         await db.execute_many(sql, values)
 
-    async def insert_data(self, table: str, **data: dict) -> Record:
+    async def insert_data(self, table: str, data: dict) -> Record:
         db = self.conn
+        data = self.__mtd(data)
         keys = data.keys()
 
         sql = f'''
@@ -63,76 +69,129 @@ class DatabaseManager:
         res = await db.execute(sql, data)
         return res
 
-    async def update_data(self, table: str, where: str, **data: dict) -> Record:
+    async def update_data(
+        self,
+        table: str,
+        where: dict[str, Any] | BaseModel,
+        upd_data: dict[str, Any] | BaseModel
+    ) -> Record:
         db = self.conn
-        do_not_update_id = data.pop('id')
-        keys = data.keys()
+        where = self.__mtd(where)
+        upd_data = self.__mtd(upd_data)
+        where_keys = where.keys()
+        upd_keys = upd_data.keys()
 
         sql = f'''
             UPDATE {table}
-            SET {', '.join([f'{i} = :{i}' for i in keys])}
-            WHERE {where}
+            SET {', '.join([f'{i} = :upd_{i}' for i in upd_keys])}
+            WHERE {' AND '.join([f'{i} = :wh_{i}' for i in where_keys])}
             RETURNING *
         '''
 
-        data.update({'id': do_not_update_id})
+        query_data = dict([
+            (f"wh_{k}", v) for k, v in where.items()
+        ] + [
+            (f"upd_{k}", v) for k, v in upd_data.items()
+        ])
 
-        res = await db.execute(sql, data)
+        res = await db.fetch_all(sql, query_data)
         return res
 
-    async def get_data_all(self, table: str, where: str = '', order_by: str = '', **data) -> list[Record]:
+    async def get_data_all(self, table: str, where: dict[str, Any] | BaseModel, order_by: List[str] = []) -> List[Record]:
         db = self.conn
-        where = await self.__format_extra_opts('WHERE', where)
-        order_by = await self.__format_extra_opts('ORDER BY', order_by)
+        where = self.__mtd(where)
+        where_query = await self.__format_extra_opts(
+            'WHERE',
+            ' AND '.join([f'{i} = :{i}' for i in where.keys()])
+        )
+        order_by = await self.__format_extra_opts(
+            'ORDER BY',
+            ', '.join([i for i in order_by])
+        )
 
         sql = f'''
             SELECT * FROM {table}
-            {where}
+            {where_query}
             {order_by}
         '''
 
-        res = await db.fetch_all(sql, data)
+        res = await db.fetch_all(
+            sql,
+            where
+        )
 
         return res
 
-    async def get_data(self, table: str, where: str = '', **data: dict) -> Record:
+    async def get_data(self, table: str, where: dict[str, Any] | BaseModel) -> Record:
         db = self.conn
-        where = await self.__format_extra_opts('WHERE', where)
+        where = self.__mtd(where)
+        where_query = await self.__format_extra_opts(
+            'WHERE',
+            ' AND '.join([f'{i} = :{i}' for i in where.keys()])
+        )
 
         sql = f'''
             SELECT * FROM {table}
-            {where}
+            {where_query}
         '''
 
-        res = await db.fetch_one(sql, data)
+        res = await db.fetch_one(sql, where)
 
         return res
 
-    async def exists_data(self, table: str, where: str = '', **data: dict) -> bool:
+    async def exists_data(self, table: str, where: dict[str, Any] | BaseModel) -> bool:
         db = self.conn
-        where = await self.__format_extra_opts('WHERE', where)
+        where = self.__mtd(where)
+        where_query = await self.__format_extra_opts(
+            'WHERE',
+            ' AND '.join([f'{i} = :{i}' for i in where.keys()])
+        )
 
         sql = f'''
             SELECT EXISTS(
                 SELECT * FROM {table}
-                {where}
+                {where_query}
             )
         '''
 
-        res = await db.fetch_one(sql, data)
+        res = await db.fetch_one(sql, where)
 
         return dict(res).get('exists')
 
-    async def delete_data(self, table: str, where: str, **data: dict):
+    async def delete_data(self, table: str, where: dict[str, Any] | BaseModel):
         db = self.conn
+        where = self.__mtd(where)
+        where_query = await self.__format_extra_opts(
+            'WHERE',
+            ' AND '.join([f'{i} = :{i}' for i in where.keys()])
+        )
 
         sql = f'''
             DELETE FROM {table}
-            WHERE {where}
+            WHERE {where_query}
         '''
 
-        await db.fetch_one(sql, data)
+        await db.fetch_one(sql, where)
+
+    @staticmethod
+    def record_to_mdl(record: Record, model: BaseModel) -> BaseModel:
+        if not issubclass(model, BaseModel):
+            raise TypeError
+        return model(**record)
+
+    @staticmethod
+    def record_to_mdls(records: Record, model: BaseModel) -> BaseModel:
+        if not issubclass(model, BaseModel):
+            raise TypeError
+        return [model(**i) for i in records]
 
     @staticmethod
     async def __format_extra_opts(opt: str, val: str) -> str:
         return f'{opt} {val}' if val != '' else val
+
+    @staticmethod
+    def __mtd(model_or_dict: BaseModel | dict):
+        # Model to dict function
+        if issubclass(type(model_or_dict), BaseModel):
+            return model_or_dict.dict(exclude_none=True)
+        return model_or_dict
